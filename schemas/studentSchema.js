35c8139a1b0b4289f9d660e1c068cd6d644ea61d
Mohammad Khashashneh <49956357+mohammadkhashashneeh@users.noreply.github.com>
2025-05-21 14:56:14 +0300

    student schema updated

--- schemas/studentSchema.js
+++ schemas/studentSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const studentSchema = new mongoose.Schema(
   {
+    user_id: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
     grade_level: Number,
     section: String,
     birth_date: Date,
