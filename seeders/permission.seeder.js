bb028640da2395c3e81ba3bda646faefda4336ae
Mohammad Khashashneh <49956357+mohammadkhashashneeh@users.noreply.github.com>
2025-05-25 21:15:27 +0300

    ..

--- seeders/permission.seeder.js
+++ seeders/permission.seeder.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Permission = require("../models/Permission");
-const { t } = require("i18next");
 
 const seedPermissions = async () => {
   try {
@@ -9,12 +8,162 @@ const seedPermissions = async () => {
     const permissionModel = new Permission();
 
     await permissionModel.deleteMany();
-    await permissionModel.insertMany([
-      { permission_key: "CREATE_USER" },
-      { permission_key: "DELETE_USER" },
-      { permission_key: "UPDATE_COURSE" },
-      { permission_key: "VIEW_REPORTS" },
-    ]);
+
+    const permissions = [
+      // Student
+      "STUDENT_VIEW",
+      "STUDENT_VIEW_DETAIL",
+      "STUDENT_CREATE",
+      "STUDENT_UPDATE",
+      "STUDENT_DELETE",
+      // Teacher
+      "TEACHER_VIEW",
+      "TEACHER_VIEW_DETAIL",
+      "TEACHER_CREATE",
+      "TEACHER_UPDATE",
+      "TEACHER_DELETE",
+      // Role
+      "ROLE_VIEW",
+      "ROLE_VIEW_DETAIL",
+      "ROLE_CREATE",
+      "ROLE_UPDATE",
+      "ROLE_DELETE",
+      // Permission
+      "PERMISSION_VIEW",
+      "PERMISSION_VIEW_DETAIL",
+      "PERMISSION_CREATE",
+      "PERMISSION_UPDATE",
+      "PERMISSION_DELETE",
+      // RolePermission
+      "ROLEPERMISSION_VIEW",
+      "ROLEPERMISSION_VIEW_DETAIL",
+      "ROLEPERMISSION_CREATE",
+      "ROLEPERMISSION_UPDATE",
+      "ROLEPERMISSION_DELETE",
+      // Course
+      "COURSE_VIEW",
+      "COURSE_VIEW_DETAIL",
+      "COURSE_CREATE",
+      "COURSE_UPDATE",
+      "COURSE_DELETE",
+      // Lesson
+      "LESSON_VIEW",
+      "LESSON_VIEW_DETAIL",
+      "LESSON_CREATE",
+      "LESSON_UPDATE",
+      "LESSON_DELETE",
+      // Subject
+      "SUBJECT_VIEW",
+      "SUBJECT_VIEW_DETAIL",
+      "SUBJECT_CREATE",
+      "SUBJECT_UPDATE",
+      "SUBJECT_DELETE",
+      // Quiz
+      "QUIZ_VIEW",
+      "QUIZ_VIEW_DETAIL",
+      "QUIZ_CREATE",
+      "QUIZ_UPDATE",
+      "QUIZ_DELETE",
+      // QuizQuestion
+      "QUIZQUESTION_VIEW",
+      "QUIZQUESTION_VIEW_DETAIL",
+      "QUIZQUESTION_CREATE",
+      "QUIZQUESTION_UPDATE",
+      "QUIZQUESTION_DELETE",
+      // Certificate
+      "CERTIFICATE_VIEW",
+      "CERTIFICATE_VIEW_DETAIL",
+      "CERTIFICATE_CREATE",
+      "CERTIFICATE_UPDATE",
+      "CERTIFICATE_DELETE",
+      // LiveSession
+      "LIVESESSION_VIEW",
+      "LIVESESSION_VIEW_DETAIL",
+      "LIVESESSION_CREATE",
+      "LIVESESSION_UPDATE",
+      "LIVESESSION_DELETE",
+      // Session
+      "SESSION_VIEW",
+      "SESSION_VIEW_DETAIL",
+      "SESSION_CREATE",
+      "SESSION_UPDATE",
+      "SESSION_DELETE",
+      // StudentProgress
+      "STUDENTPROGRESS_VIEW",
+      "STUDENTPROGRESS_VIEW_DETAIL",
+      "STUDENTPROGRESS_CREATE",
+      "STUDENTPROGRESS_UPDATE",
+      "STUDENTPROGRESS_DELETE",
+      // Attendance
+      "ATTENDANCE_VIEW",
+      "ATTENDANCE_VIEW_DETAIL",
+      "ATTENDANCE_CREATE",
+      "ATTENDANCE_UPDATE",
+      "ATTENDANCE_DELETE",
+      // Enrollment
+      "ENROLLMENT_VIEW",
+      "ENROLLMENT_VIEW_DETAIL",
+      "ENROLLMENT_CREATE",
+      "ENROLLMENT_UPDATE",
+      "ENROLLMENT_DELETE",
+      // Submission
+      "SUBMISSION_VIEW",
+      "SUBMISSION_VIEW_DETAIL",
+      "SUBMISSION_CREATE",
+      "SUBMISSION_UPDATE",
+      "SUBMISSION_DELETE",
+      // StudentQuizAttempt
+      "STUDENTQUIZATTEMPT_VIEW",
+      "STUDENTQUIZATTEMPT_VIEW_DETAIL",
+      "STUDENTQUIZATTEMPT_CREATE",
+      "STUDENTQUIZATTEMPT_UPDATE",
+      "STUDENTQUIZATTEMPT_DELETE",
+      // Message
+      "MESSAGE_VIEW",
+      "MESSAGE_VIEW_DETAIL",
+      "MESSAGE_CREATE",
+      "MESSAGE_UPDATE",
+      "MESSAGE_DELETE",
+      // Media
+      "MEDIA_VIEW",
+      "MEDIA_VIEW_DETAIL",
+      "MEDIA_CREATE",
+      "MEDIA_UPDATE",
+      "MEDIA_DELETE",
+      // Device
+      "DEVICE_VIEW",
+      "DEVICE_VIEW_DETAIL",
+      "DEVICE_CREATE",
+      "DEVICE_UPDATE",
+      "DEVICE_DELETE",
+      // Transaction
+      "TRANSACTION_VIEW",
+      "TRANSACTION_VIEW_DETAIL",
+      "TRANSACTION_CREATE",
+      "TRANSACTION_UPDATE",
+      "TRANSACTION_DELETE",
+      // Review
+      "REVIEW_VIEW",
+      "REVIEW_CREATE",
+      "REVIEW_UPDATE",
+      "REVIEW_DELETE",
+      // BlockedTokens
+      "BLOCKEDTOKENS_VIEW",
+      "BLOCKEDTOKENS_VIEW_DETAIL",
+      "BLOCKEDTOKENS_CREATE",
+      "BLOCKEDTOKENS_DELETE",
+      // Address
+      "ADDRESS_VIEW",
+      "ADDRESS_VIEW_DETAIL",
+      "ADDRESS_CREATE",
+      "ADDRESS_UPDATE",
+      "ADDRESS_DELETE",
+      // Auth
+      "AUTH_LOGOUT",
+    ];
+
+    const permissionDocs = permissions.map((key) => ({ permission_key: key }));
+    await permissionModel.insertMany(permissionDocs);
 
     console.log("Permissions seeded");
 
--- seeders/rolePermission.seeder.js
+++ seeders/rolePermission.seeder.js
@@ -1,55 +1,158 @@
 const mongoose = require("mongoose");
 const RolePermission = require("../models/RolePermission");
-const { faker } = require("@faker-js/faker");
-const { t } = require("i18next");
 
 const seedRolePermissions = async (roles, permissions) => {
   try {
     console.log("Seeding role permissions...");
+
     const rolePermissionModel = new RolePermission();
     await rolePermissionModel.deleteMany({});
 
-    console.log(
-      "Roles:",
-      roles.map((r) => r.name)
-    );
-    console.log(
-      "Permissions:",
-      permissions.map((p) => p.permission_key)
-    );
+    const getPermissionIds = (keys) =>
+      keys
+        .map((key) => permissions.find((p) => p.permission_key === key)?._id)
+        .filter(Boolean);
 
-    const rolePermissions = [
-      {
-        role_id: roles.find((r) => r.name === "admin")._id,
-        permission_id: permissions.find(
-          (p) => p.permission_key === "CREATE_USER"
-        )._id,
-      },
-      {
-        role_id: roles.find((r) => r.name === "admin")._id,
-        permission_id: permissions.find(
-          (p) => p.permission_key === "UPDATE_COURSE"
-        )._id,
-      },
-      {
-        role_id: roles.find((r) => r.name === "teacher")._id,
-        permission_id: permissions.find(
-          (p) => p.permission_key === "UPDATE_COURSE"
-        )._id,
-      },
-      {
-        role_id: roles.find((r) => r.name === "student")._id,
-        permission_id: permissions.find(
-          (p) => p.permission_key === "VIEW_REPORTS"
-        )._id,
-      },
+    const roleMap = Object.fromEntries(roles.map((r) => [r.name, r._id]));
+
+    const adminPermissions = permissions.map((p) => ({
+      role_id: roleMap["admin"],
+      permission_id: p._id,
+    }));
+
+    const teacherPermissionKeys = [
+      // core teaching and session management
+      "COURSE_VIEW",
+      "COURSE_VIEW_DETAIL",
+      "COURSE_CREATE",
+      "COURSE_UPDATE",
+      "LESSON_VIEW",
+      "LESSON_VIEW_DETAIL",
+      "LESSON_CREATE",
+      "LESSON_UPDATE",
+      "LESSON_DELETE",
+      "SUBJECT_VIEW",
+      "SUBJECT_VIEW_DETAIL",
+      "SUBJECT_CREATE",
+      "SUBJECT_UPDATE",
+      "SUBJECT_DELETE",
+      "QUIZ_VIEW",
+      "QUIZ_VIEW_DETAIL",
+      "QUIZ_CREATE",
+      "QUIZ_UPDATE",
+      "QUIZ_DELETE",
+      "QUIZQUESTION_VIEW",
+      "QUIZQUESTION_VIEW_DETAIL",
+      "QUIZQUESTION_CREATE",
+      "QUIZQUESTION_UPDATE",
+      "QUIZQUESTION_DELETE",
+      "CERTIFICATE_VIEW",
+      "CERTIFICATE_VIEW_DETAIL",
+      "CERTIFICATE_CREATE",
+      "CERTIFICATE_UPDATE",
+      "CERTIFICATE_DELETE",
+      "LIVESESSION_VIEW",
+      "LIVESESSION_VIEW_DETAIL",
+      "LIVESESSION_CREATE",
+      "LIVESESSION_UPDATE",
+      "LIVESESSION_DELETE",
+      "ATTENDANCE_VIEW",
+      "ATTENDANCE_VIEW_DETAIL",
+      "ATTENDANCE_CREATE",
+      "ATTENDANCE_UPDATE",
+      "ATTENDANCE_DELETE",
+      "STUDENTPROGRESS_VIEW",
+      "STUDENTPROGRESS_VIEW_DETAIL",
+      "STUDENTPROGRESS_CREATE",
+      "STUDENTPROGRESS_UPDATE",
+      "STUDENTPROGRESS_DELETE",
+      "SUBMISSION_VIEW",
+      "SUBMISSION_VIEW_DETAIL",
+      "SUBMISSION_UPDATE",
+      "SUBMISSION_DELETE",
+      "STUDENTQUIZATTEMPT_VIEW",
+      "STUDENTQUIZATTEMPT_VIEW_DETAIL",
+      "STUDENTQUIZATTEMPT_UPDATE",
+      "STUDENTQUIZATTEMPT_DELETE",
+      "MESSAGE_VIEW",
+      "MESSAGE_VIEW_DETAIL",
+      "MESSAGE_CREATE",
+      "MESSAGE_UPDATE",
+      "MESSAGE_DELETE",
+      "MEDIA_VIEW",
+      "MEDIA_VIEW_DETAIL",
+      "MEDIA_CREATE",
+      "MEDIA_UPDATE",
+      "MEDIA_DELETE",
+      "TEACHER_VIEW",
+      "TEACHER_VIEW_DETAIL",
+      "TEACHER_UPDATE",
+      "SESSION_VIEW",
+      "SESSION_VIEW_DETAIL",
+      "REVIEW_VIEW",
+      "AUTH_LOGOUT",
     ];
 
-    const insertedRolePermissions = await rolePermissionModel.insertMany(
-      rolePermissions
-    );
-    console.log(`Inserted ${insertedRolePermissions.length} role permissions`);
-    return insertedRolePermissions;
+    const studentPermissionKeys = [
+      "COURSE_VIEW",
+      "COURSE_VIEW_DETAIL",
+      "LESSON_VIEW",
+      "LESSON_VIEW_DETAIL",
+      "SUBJECT_VIEW",
+      "SUBJECT_VIEW_DETAIL",
+      "ENROLLMENT_CREATE",
+      "ENROLLMENT_VIEW",
+      "ENROLLMENT_VIEW_DETAIL",
+      "QUIZ_VIEW",
+      "QUIZ_VIEW_DETAIL",
+      "STUDENTQUIZATTEMPT_CREATE",
+      "STUDENTQUIZATTEMPT_VIEW",
+      "STUDENTQUIZATTEMPT_VIEW_DETAIL",
+      "SUBMISSION_CREATE",
+      "SUBMISSION_VIEW",
+      "SUBMISSION_VIEW_DETAIL",
+      "STUDENTPROGRESS_VIEW",
+      "STUDENTPROGRESS_VIEW_DETAIL",
+      "CERTIFICATE_VIEW",
+      "CERTIFICATE_VIEW_DETAIL",
+      "MESSAGE_VIEW",
+      "MESSAGE_VIEW_DETAIL",
+      "MESSAGE_CREATE",
+      "MESSAGE_UPDATE",
+      "MESSAGE_DELETE",
+      "REVIEW_VIEW",
+      "REVIEW_CREATE",
+      "REVIEW_UPDATE",
+      "REVIEW_DELETE",
+      "TRANSACTION_CREATE",
+      "TRANSACTION_VIEW",
+      "TRANSACTION_VIEW_DETAIL",
+      "STUDENT_VIEW",
+      "STUDENT_VIEW_DETAIL",
+      "STUDENT_UPDATE",
+      "ADDRESS_CREATE",
+      "ADDRESS_VIEW",
+      "ADDRESS_VIEW_DETAIL",
+      "ADDRESS_UPDATE",
+      "ADDRESS_DELETE",
+      "AUTH_LOGOUT",
+    ];
+
+    const rolePermissions = [
+      ...adminPermissions,
+      ...getPermissionIds(teacherPermissionKeys).map((pid) => ({
+        role_id: roleMap["teacher"],
+        permission_id: pid,
+      })),
+      ...getPermissionIds(studentPermissionKeys).map((pid) => ({
+        role_id: roleMap["student"],
+        permission_id: pid,
+      })),
+    ];
+
+    const inserted = await rolePermissionModel.insertMany(rolePermissions);
+    console.log(`Inserted ${inserted.length} role permissions`);
+    return inserted;
   } catch (error) {
     console.error("Error seeding role permissions:", error);
     throw error;
